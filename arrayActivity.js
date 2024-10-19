30ccac6a7ca697df444d1b49b0cd0ea65a39a716
LOVE-AMOR <loveamorbalaqui2@gmail.com>
2024-10-18 19:43:16 -0800

    Array Activity

---
+++ arrayActivity.js
@@ -0,0 +1,10 @@
+const favSingers = ["Bruno Mars", "Jason Derulo", "Billie Eilish"];
+console.log(favSingers[2]); 
+
+const favNumbers = [10, 16, 3, 25];
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr[0]); 
+console.log(mixedArr[1]); 
+console.log(mixedArr[2]); 
+console.log(mixedArr[3]); 
